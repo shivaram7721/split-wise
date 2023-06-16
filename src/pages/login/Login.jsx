bb59ee0eab33c77345e55d9362efa43fcecafd17
nlprasanna5 <lakshmi.76740@gmail.com>
2023-06-16 22:07:55 +0530

    created login page

--- src/pages/login/Login.jsx
+++ src/pages/login/Login.jsx
@@ -1,8 +1,51 @@
-import React from 'react'
+import React from 'react';
+import styles from './Login.module.css';
+import { Navbar } from '../../components/Navbar/Navbar';
 
 function Login() {
   return (
-    <div>Login</div>
+    <>
+      <Navbar />
+     
+      <div className={styles.loginBackground}>
+        <div className={styles.loginContent}>
+          <div className={styles.loginContainer}>
+          {/* <div className={styles.formContainer}> */}
+            <form className={styles.loginForm}>
+             
+              <div className={styles.loginText}>
+                <p className={styles.loginTitle}>Log in</p>
+              </div>
+
+              <div className={styles.formGroup}>
+                <label className={styles.label}>Email address</label>
+                <input type='text' name='email' required className={styles.loginInput}/>
+              </div>
+              <div className={styles.formGroup}>
+                <label className={styles.label}>Password</label>
+                <input type='password' name='password' required className={styles.loginInput} />
+              </div>
+              <div className={styles.loginBtn}>
+                <button className={`${styles.login} ${styles.btnStyle}`}>Log in</button>
+              </div>
+              <div className={styles.reset}>
+                <p className={styles.forgotPassword}>Forgot your password?</p>
+              </div>
+              <div className={styles.reset}>
+               or
+              </div>
+
+              <div className={styles.googleAuth}>
+                <button className={`${styles.googleLogin} ${styles.btnStyle}`}>Sign in with Google</button>
+              </div>
+              
+            </form>
+            {/* </div> */}
+          </div>
+        </div>
+      </div>
+     
+    </>
   )
 }
 
